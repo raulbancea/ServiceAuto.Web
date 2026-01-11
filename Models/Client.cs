f77274f6d1e176fbd209eefa5789a59638dedffc
raulbancea <raul.bancea@stud.ubbcluj.ro>
2026-01-11 11:15:38 +0200

    Add Client domain model

---
+++ Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceAuto.Web.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nume { get; set; }
+
+        [Phone]
+        public string Telefon { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(200)]
+        public string Adresa { get; set; }
+
+        [Required]
+        public string TipClient { get; set; } // Persoana Fizica / Juridica
+    }
+}
