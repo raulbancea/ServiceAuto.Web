c5553a8e0e273654a34bbb10dcdeb47c38963361
raulbancea <raul.bancea@stud.ubbcluj.ro>
2026-01-11 11:07:24 +0200

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceAuto.Web.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
